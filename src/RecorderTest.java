ab4c9e376502eb4786668aef5036a98b492874e5
Axel Hodler <derxor@gmail.com>
2017-03-18 14:43:28 +0100

    can record multiple requests

--- src/RecorderTest.java
+++ src/RecorderTest.java
@@ -1,5 +1,8 @@
 import org.junit.Test;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
@@ -11,12 +14,22 @@ public class RecorderTest {
     assertThat(r.replay("http://foo.org"), is("response"));
   }
 
+  @Test
+  public void canRecordMultipleRequests() {
+    Recorder r = new Recorder();
+    r.record("http://bar.org", "anotherResponse");
+    assertThat(r.replay("http://bar.org"), is("anotherResponse"));
+  }
+
   private class Recorder {
+    private Map<String, String> recordings = new HashMap<>();
+
     public void record(String url, String response) {
+      recordings.put(url, response);
     }
 
-    public String replay(String s) {
-      return "response";
+    public String replay(String url) {
+      return recordings.get(url);
     }
   }
 }
