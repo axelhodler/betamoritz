64dd5594ce7d289ac59006f314a26995f3138e49
Axel Hodler <derxor@gmail.com>
2017-03-18 21:29:10 +0100

    you dont know the response yet

--- src/RecorderTest.java
+++ src/RecorderTest.java
@@ -14,7 +14,7 @@ public class RecorderTest {
 
   @Before
   public void initialize() {
-    r = new Recorder();
+    r = new Recorder(null);
   }
 
   @Test
@@ -25,8 +25,22 @@ public class RecorderTest {
     assertThat(r.replay(new Request("GET", url)), is("anotherResponse"));
   }
 
+  @Test
+  public void responseComesFromExternalService() {
+    HttpGatewayStub httpGatewayStub = new HttpGatewayStub();
+    r = new Recorder(httpGatewayStub);
+    URL url = new URL("http://bar.org");
+    r.record(new Request("GET", url));
+    assertThat(r.replay(new Request("GET", url)), is("stubbedResponse"));
+  }
+
   private class Recorder {
     private Map<Request, String> recordings = new HashMap<>();
+    private HttpGatewayStub httpGatewayStub;
+
+    public Recorder(HttpGatewayStub httpGatewayStub) {
+      this.httpGatewayStub = httpGatewayStub;
+    }
 
     public void record(Request request, String response) {
       recordings.put(request, response);
@@ -35,6 +49,15 @@ public class RecorderTest {
     public String replay(Request request) {
       return recordings.get(request);
     }
+
+    public void record(Request get) {
+      recordings.put(get, httpGatewayStub.get(get));
+    }
   }
 
+  private class HttpGatewayStub {
+    public String get(Request r) {
+      return "stubbedResponse";
+    }
+  }
 }
